bee37fe1c9e1aa812bd0d4397a04e276c2553cf6
MrFurphie <hfelsman@gmail.com>
2023-04-06 14:32:45 -0400

    Add files via upload

---
+++ src/routes/catfacts/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let data;
+  const {catpics} = data;
+</script>
+
+<article>
+  {#each catpics as { caption }}
+  <figure>
+    <img src="https://cataas.com/cat/says/{caption}" alt="">
+  </figure>
+  {/each}
+</article>
+
+
+<style>
+  article {
+    padding: 2em;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 2em;
+  }
+</style>
