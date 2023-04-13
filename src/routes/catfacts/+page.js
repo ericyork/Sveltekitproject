7d6a36c71eea0687890c35e5ec9c8f3e25d8a8e1
ericyork <eyork@clarkson.edu>
2023-04-13 13:20:05 -0400

    working apit

--- src/routes/catfacts/+page.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch, params }) {
-    const res = await fetch(`https://cataas.com/cat/says/${params.id}`);
-    const item = await res.json();
-   
-    return { item };
-  }
--- src/routes/catfacts/+page.svelte
+++ src/routes/catfacts/+page.svelte
@@ -1,22 +1,27 @@
 <script>
+  import { add_attribute } from 'svelte/internal';
+
   export let data;
-  const {catpics} = data;
+  const {catResp} = data;
+  console.log(catResp);
 </script>
 
-<!-- <article>
-  {#each catpics as { caption }}
-  <figure>
-    <img src="https://cataas.com/cat/says/{caption}" alt="">
-  </figure>
-  {/each}
-</article> -->
+<article>
+  <h2>Here you go!</h2>
+  <a href="/catfacts">Refresh</a>
+  <img src="https://cataas.com/{catResp.url}" alt="">
+  <p>File type: {catResp.mimetype}</p>
+
+</article>
 
 
 <style>
   article {
-    padding: 2em;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 2em;
+    display: flex;
+    flex-direction: column;
+  }
+  img {
+    width: 50%;
+    height: auto;
   }
 </style>
